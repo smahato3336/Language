b466a584d49aa39725356f38930d809e2ce740e2
smahato3336 <smahato3336@gmail.com>
2025-10-07 22:31:17 +0530

    Add files via upload

---
+++ .devcontainer/c_language.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<math.h>
+/*
+//Check if a number is divisible by 2 or not.
+int main() {
+    int x;
+    printf("enter a number: ");
+    scanf("%d", & x);
+    printf("%d", x % 2 == 0);
+    return 0;
+}
+
+//Write a program to calculate the area of a circle (radius is given).
+
+int main () {
+    float radius;
+    printf("Enter radius of circle: \n");
+    scanf("%f", & radius);
+    printf("The area of circle is : %f \n", 3.14 * radius * radius);
+    return 0;
+}   
+//Write a program to calculate the area of a square. (Side is given)
+
+int main(){
+    float side;
+    printf("enter side of square : ");
+    scanf("%f", &side);
+    printf("area of a square is %f \n", side * side);
+}
+//Write a program to calculate the perimeter of a rectangle. Take sides, a & b, from the user.
+int main() {
+    float length;
+    float width;
+    
+    printf("enter the length of rectangle: ");
+    scanf("%f", &length);
+    printf("enter the width of rectangle: ");
+    
+    scanf("%f", &width);
+    printf("The perimeter of rectangle is %f \n", 2 * (length + width));
+    return 0;
+}
+//Take a number (n) from user & output its cube (n*n*n).
+
+int main() {
+    int n, cube;
+    printf("Please enter a value :");
+    
+    scanf("%d", &n);
+    cube = n * n * n;
+    
+    printf("The cube of %d is %d \n", n, cube);
+    return 0;
+}
+//int  
+int main() {
+    int a = (int) 1.99999;
+    printf("%d \n", a);
+    return 0;
+}
+//float
+int main() {
+    float a = 1.99999;
+    printf("%f", a);
+    return 0;
+}
+int main(){
+    int b, c;
+    b = c = 1;
+    int a = b + 1;
+    int power = pow(b,c);
+    printf("%d \n", a);
+    printf("%d", power);
+    return 0;
+}
+//Modulo
+int main(){
+    //printf("%d", 16 % 10);
+    //printf("%d", -6 % 5); //if negetive value comes in numerator then, output will be negetive. But if negetive value cones in denumerator and positive value in numerator, output will be positive)
+    //printf("%d", 7 % -3);
+    return 0;
+}
+*/
+
+// Arithmatic Instructions
+/*
+int main() {
+    //printf("%d", 2 * 2);
+    //printf("%f", 4.0 * 2);
+    printf("%f", 6.5 * 3.5);
+    return 0;
+}
+*/
+
+// ************ BODMAS Rule *************
+// int main() {
+//     int a = 5 * 2 - 2 * 3;
+//     printf("%d", a);
+//     return 0;
+// }
+
+// ************Operator Precedence (BODMAS - Priority Operator) ************
+// int main() {
+//     int a = 5 * 2 / 2 * 3; //Left to right
+//     printf("%d", a);
+//     return 0;
+// }
+
+
+// int main(){
+//     int a = 5 * (2/2) * 3;
+//     printf("%d", a);
+//     return 0;
+// }
