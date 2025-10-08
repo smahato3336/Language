a439f52cb5a88673b6c96493daa1352fbb957079
smahato3336 <smahato3336@gmail.com>
2025-10-08 07:38:22 +0530

    Update c_language.c

--- .devcontainer/c_language.c
+++ .devcontainer/c_language.c
@@ -112,3 +112,72 @@ int main() {
 //     printf("%d", a);
 //     return 0;
 // }
+// int main() {
+//     double base = 2; // Base value
+//     double exponent; // Exponent value
+
+//     // Prompt user for input
+//     printf("Enter the exponent: ");
+//     scanf("%lf", &exponent); // Read user input
+
+//     // Calculate power
+//     double result = pow(base, exponent);
+
+//     // Output the result
+//     printf("%.0f raised to the power of %.0f is %.0f\n", base, exponent, result);
+
+//     return 0;
+// }
+//If today is sunday and snowing, then true.
+// int main(){
+//     int isSunday = 1;
+//     int isSnowing = 1;
+//     printf("%d \n", isSunday && isSnowing);
+//     return 0;
+// }
+
+// int main(){
+//     int isSunday = 0;
+//     int isSnowing = 1;
+//     printf("%d \n", isSunday && isSnowing);
+//     return 0;
+// }
+
+//If it's sunday or it's raining -> True
+
+// int main(){
+//     int isSunday = 1;
+//     int isRaining = 0;
+//     printf("%d", isSunday || isRaining);
+//     return 0;
+// }
+//If you age > 18 & <100 you can drive.
+// int main(){
+//     int age;
+//     printf("Enter your age here: ");
+//     scanf("%d", &age);
+
+//     if(age > 18 && age < 100){
+//         printf("You can drive");
+//     }else{
+//         printf("You can not drive");
+//     }
+//     return 0;
+// }
+
+// Write a program to print the average of 3 numbers.
+
+// int main(){
+//    float num1, num2, num3;
+//    printf("Enter the First Number: ");
+//    scanf("%f", &num1);
+
+//    printf("Enter the Second Number: ");
+//    scanf("%f", &num2);
+
+//    printf("Enter the Third Number: ");
+//    scanf("%f", &num3);
+
+//    printf("Average of three numbers is: %f \n", (num1 + num2 + num3)/3);
+//    return 0;
+// }
